e68222482e78ba473b4849852b754ff8a95ca1d5
gabriel1og <gogomide.go@gmail.com>
2023-08-12 01:24:23 -0300

    adding wpp icon

--- rodrigo-taxista-para-cadeirantes/components/Contact/page.tsx
+++ rodrigo-taxista-para-cadeirantes/components/Contact/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Flex, Box, Badge, Text, Heading, Divider } from "@chakra-ui/react";
 import Link from "next/link";
+import { BsWhatsapp } from "react-icons/bs";
 
 export function Contact() {
   return (
@@ -22,9 +23,13 @@ export function Contact() {
         <Link href="https://api.whatsapp.com/send/?phone=5531995622051&text&type=phone_number&app_absent=0">
           <Badge
             colorScheme="green"
+            display="flex"
+            flexDir="row"
+            alignItems="center"
             py={5}
             px={[24, 24, 24, 28]}
             borderRadius={64}
+            color="#d8e9ff"
             backdropFilter="blur(16px) saturate(180%)"
             backgroundColor="rgba(34, 57, 82, 0.75)"
             _hover={{
@@ -32,7 +37,8 @@ export function Contact() {
               boxShadow: "0px 0px 10px 3px #9fb2bc",
             }}
           >
-            <Text fontSize="1.1rem" color="#d8e9ff">
+            <BsWhatsapp size={20} />
+            <Text ml={2} fontSize="1.1rem">
               Whatsapp
             </Text>
           </Badge>
